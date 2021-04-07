2e708edfd3fd33dd719c937eaf16c9093b089ded
NyaNya <76605346+SleepyCatgirl@users.noreply.github.com>
2021-04-07 23:28:16 +0200

    Font related changes

--- config.el
+++ config.el
@@ -26,8 +26,10 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-palenight)
+;; Set font. Pretty straighforward.
 (setq doom-font "SF Mono Powerline")
-
+;; Set font for japanese letters specifically, not touching other
+(set-fontset-font nil 'kana "IPAGothic")
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
